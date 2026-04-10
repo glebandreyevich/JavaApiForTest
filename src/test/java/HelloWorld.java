669106fd16e97c10c70620fc83e908194cd246f9
glebandreyevich <faded5103@gmail.com>
2026-04-10 18:06:01 +0300

    First api test

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
-
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 public class HelloWorld {
 
     @Test
     public void HelloWorld() {
-        System.out.println("Hello World");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+                response.prettyPrint();
+
     }
 }
