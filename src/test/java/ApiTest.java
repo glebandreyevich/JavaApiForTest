7a0aa87719d2fb483b5a8490c67f3052617d549c
glebandreyevich <faded5103@gmail.com>
2026-04-14 16:56:05 +0300

    training

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApiTest {
+
+}
