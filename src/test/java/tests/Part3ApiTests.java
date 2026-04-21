1833eb7cbbb6821445728a9234fd50e44cd4bb24
glebandreyevich <faded5103@gmail.com>
2026-04-21 17:27:36 +0300

    Ex13: User Agent

--- src/test/java/tests/Part3ApiTests.java
+++ src/test/java/tests/Part3ApiTests.java
@@ -38,4 +38,36 @@ public class Part3ApiTests extends BaseTestCase {
                 getHeader(responseHeader, "x-secret-homework-header"),
                 "Header 'x-secret-homework-header' has wrong value");
     }
+    //User Agent
+    public record ExpectedResult(String platform, String browser, String device) {
+    }
+
+    static Stream<Arguments> userAgentDataProvider() {
+        return Stream.of(Arguments.of("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
+                        , new ExpectedResult("Mobile", "No", "Android"))
+                , Arguments.of("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"
+                        , new ExpectedResult("Mobile", "Chrome", "iOS"))
+                , Arguments.of("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html"
+                        , new ExpectedResult("Googlebot", "Unknown", "Unknown"))
+                , Arguments.of("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"
+                        , new ExpectedResult("Web", "Chrome", "No"))
+                , Arguments.of("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+                        , new ExpectedResult("Mobile", "No", "iPhone"))
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("userAgentDataProvider")
+    public void UserAgentTest(String userAgent, ExpectedResult ExpectedResult) {
+        Response response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check");
+        assertAll("For User-Agent " + userAgent + " expected result does not match actual result" ,
+                () -> assertEquals(ExpectedResult.platform, getStringFromJson(response, "platform")),
+                () -> assertEquals(ExpectedResult.browser, getStringFromJson(response, "browser")),
+                () -> assertEquals(ExpectedResult.device, getStringFromJson(response, "device"))
+        );
+    }
 }
